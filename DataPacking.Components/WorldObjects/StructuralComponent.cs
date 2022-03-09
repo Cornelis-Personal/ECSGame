f603e79d1acb21b83792762c5851ccfbd7b58446
Cornelis-Personal <46776364+Cornelis-Personal@users.noreply.github.com>
2022-03-10 07:26:26 +1000

    Initial Commit

---
+++ DataPacking.Components/WorldObjects/StructuralComponent.cs
@@ -0,0 +1,16 @@
+﻿using ECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPacking.Components.WorldObjects
+{
+    public class StructuralComponent : Component
+    {
+        public float Health;
+        public float Armor;
+        public float Shield;
+    }
+}
---
+++ DataPacking.Components/WorldObjects/TransformComponent.cs
@@ -0,0 +1,12 @@
+﻿
+using ECS;
+using System.Numerics;
+
+namespace DataPacking.Components.WorldObjects
+{
+    public class TransformComponent : Component
+    {
+        public Vector2 Position;
+        public Quaternion Rotation;
+    }
+}
